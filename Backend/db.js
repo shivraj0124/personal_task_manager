11e5387d7b22c209f6ca57cb1113ef21be62f69f
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2023-11-29 09:54:30 +0530

    Delete Backend directory

--- Backend/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql2");
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Shivraj#2501",
-  database: "taskmanagement",
-});
-
-module.exports=db
